b8710854a09e899ba5f1f77f8f1c9eeef600d668
Darshan Shakya <darshan.signups@gmail.com>
2024-04-17 15:18:45 -0700

    add isBlind check as well

--- scripts/module.js
+++ scripts/module.js
@@ -3,19 +3,19 @@ Hooks.once("init", () => {
 });
 
 Hooks.on("preCreateChatMessage", async (message) => {
-  console.log("===in pre=== ");
-  console.log(message);
   const outcome = message?.flags?.pf2e?.context?.outcome;
-  // const isBlind = message.blind;
-  console.log("====preCreateChatMessage====", outcome);
-  console.log("ACTOR", message.actor);
+  const isBlind = message.blind;
 
   const fileFolderName = "upload/Images/Crit";
-  const critFilename = `${message.actor.name}_crit.gif`;
-  const bgImg = `${fileFolderName}/${critFilename}`;
-  console.log("bgImg", bgImg);
+  /* Make names obvious: lowercase all + join with underscore. */
+  const critFilename = `${message.actor.name
+    .toLowerCase()
+    .split(" ")
+    .join("_")}_crit.gif`;
 
-  if (outcome === "criticalSuccess") {
+  const bgImg = `${fileFolderName}/${critFilename}`;
+
+  if (outcome === "criticalSuccess" && !isBlind) {
     // const bgImg = imageFiles[Math.floor(Math.random() * imageFiles.length)];
     // your macro here, and just put bgImg well at bgImg in your large object.
     game.modules.get("scene-transitions").api.macro(

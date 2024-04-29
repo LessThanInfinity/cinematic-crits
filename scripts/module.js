52c0ee3437f91e3f4dd5d8b4c228bbac8e9dbacc
Darshan Shakya <darshan.signups@gmail.com>
2024-04-29 07:38:46 -0700

    first iteration confirmed working - directories created, and expected to have sounds

--- scripts/module.js
+++ scripts/module.js
@@ -27,7 +27,7 @@ const createImagesDir = async () => {
     await FilePicker.createDirectory("data", "cinematic_crits/upload/images");
   } catch (err) {
     // console.error(err);
-    console.log("Cinematic: Upload directory already exists");
+    console.log("Cinematic: Upload/images directory already exists");
   }
 };
 
@@ -36,7 +36,7 @@ const createSoundsDir = async () => {
     await FilePicker.createDirectory("data", "cinematic_crits/upload/sounds");
   } catch (err) {
     // console.error(err);
-    console.log("Cinematic: Upload directory already exists");
+    console.log("Cinematic: Upload/sounds directory already exists");
   }
 };
 
@@ -52,6 +52,51 @@ Hooks.once("init", async () => {
   await createCinematicCritDirectories();
 });
 
+const getCritImageForActor = async (actorName) => {
+  const imageFileName = `${actorName.toLowerCase().split(" ").join("_")}.gif`;
+  return `cinematic_crits/upload/images/${imageFileName}`;
+
+  // TODO: consider whether we want to allow multiple images under a directory.
+  // const critImagePath = await FilePicker.browse(
+  //   "data",
+  //   `cinematic_crits/upload/images/${imageFileName}`
+  // );
+  // return critImagePath;
+};
+
+const getCritSoundForActor = async (actorName) => {
+  const audioDirectoryName = `${actorName.toLowerCase().split(" ").join("_")}`;
+  const defaultCritSound =
+    "modules/cinematic-crits/assets/sfx/default_crit.wav";
+  try {
+    const fileList = await FilePicker.browse(
+      "data",
+      `cinematic_crits/upload/sounds/${audioDirectoryName}`
+    );
+    const audioFiles = fileList.files.filter((f) =>
+      AudioHelper.hasAudioExtension(f)
+    );
+    const critAudio = audioFiles[Math.floor(Math.random() * audioFiles.length)];
+
+    /* If no files exist, send back default sound.  */
+    if (critAudio) {
+      return critAudio;
+    } else {
+      console.log(
+        "cinematic: No files in actor audio directory. Using default crit sound"
+      );
+      return defaultCritSound;
+    }
+  } catch (err) {
+    console.error("Cinematic: Audio fetch:", err);
+
+    // send back default if error or cannot find directory
+    console.log("cinematic: Using default crit sound");
+
+    return defaultCritSound;
+  }
+};
+
 Hooks.on("preCreateChatMessage", async (message) => {
   const outcome = message?.flags?.pf2e?.context?.outcome;
   const isBlind = message.blind;
@@ -64,12 +109,19 @@ Hooks.on("preCreateChatMessage", async (message) => {
     .split(" ")
     .join("_")}.gif`;
 
-  const bgImg = `${fileFolderName}/${critFilename}`;
-  const defaultCritSound =
-    "modules/cinematic-crits/assets/sfx/default_crit.wav";
-  const audioFilePath = "upload/Sounds/Crit/default_crit.wav";
-  console.log("audioFilePath", audioFilePath);
-  console.log("defaultCritSound", defaultCritSound);
+  // const bgImg = `${fileFolderName}/${critFilename}`;
+
+  const bgImg = await getCritImageForActor(message.actor.name);
+  const audio = await getCritSoundForActor(message.actor.name);
+
+  // console.log("defaultCritSound", defaultCritSound);
+  console.log("cinematic: bgImg", bgImg);
+  console.log("cinematic: audio", audio);
+
+  // const audioFileList = await FilePicker.browse("data", 'upload/Images/Crit');
+
+  // const imageFiles = fileList.files.filter(f => ImageHelper.hasImageExtension(f) || VideoHelper.hasVideoExtension(f));
+  // const bgImg = imageFiles[Math.floor(Math.random() * imageFiles.length)];
 
   if (outcome === "criticalSuccess" && !isBlind) {
     // const bgImg = imageFiles[Math.floor(Math.random() * imageFiles.length)];
@@ -92,7 +144,7 @@ Hooks.on("preCreateChatMessage", async (message) => {
         fadeIn: 400,
         delay: 2000,
         fadeOut: 400,
-        audio: defaultCritSound,
+        audio,
         skippable: true,
         audioLoop: false,
         gmHide: false,

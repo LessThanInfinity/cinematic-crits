4b8ee11480c690cf62e001ac8594d827dd8a1b46
Darshan Shakya <darshan.signups@gmail.com>
2024-04-29 07:09:25 -0700

    create directories in foundry, maybe not in forge?

--- scripts/module.js
+++ scripts/module.js
@@ -1,5 +1,55 @@
-Hooks.once("init", () => {
-  console.log("Shooting some cinmatic crits!");
+// const fs = require("fs");
+
+// const MAIN_DIRECTORY = "cinematic_crits/upload";
+// const IMAGE_DIRECTORY = "cinematic_crits/upload/images";
+// const SOUND_DIRECTORY = "cinematic_crits/upload/sounds";
+
+const createMainDir = async () => {
+  try {
+    await FilePicker.createDirectory("data", "cinematic_crits");
+  } catch (err) {
+    // console.error(err);
+    console.log("Cinematic: main directory already exists");
+  }
+};
+
+const createUploadDir = async () => {
+  try {
+    await FilePicker.createDirectory("data", "cinematic_crits/upload");
+  } catch (err) {
+    // console.error(err);
+    console.log("Cinematic: Upload directory already exists");
+  }
+};
+
+const createImagesDir = async () => {
+  try {
+    await FilePicker.createDirectory("data", "cinematic_crits/upload/images");
+  } catch (err) {
+    // console.error(err);
+    console.log("Cinematic: Upload directory already exists");
+  }
+};
+
+const createSoundsDir = async () => {
+  try {
+    await FilePicker.createDirectory("data", "cinematic_crits/upload/sounds");
+  } catch (err) {
+    // console.error(err);
+    console.log("Cinematic: Upload directory already exists");
+  }
+};
+
+const createCinematicCritDirectories = async () => {
+  await createMainDir();
+  await createUploadDir();
+  await createImagesDir();
+  await createSoundsDir();
+};
+
+Hooks.once("init", async () => {
+  console.log("Cinematic: Shooting some cinmatic crits!");
+  await createCinematicCritDirectories();
 });
 
 Hooks.on("preCreateChatMessage", async (message) => {
